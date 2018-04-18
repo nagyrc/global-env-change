c285416e3abb65ed25b7a0fb5984a38c84156577
nagyrc <nagyrc@gmail.com>
2018-04-18 14:00:12 -0600

    working on code

--- import_raw_data.R
+++ import_raw_data.R
@@ -2,6 +2,24 @@
 
 #start by importing .csv files
 #import all files in folder
+#change working directory
+#setwd("data/")
+
+#setwd("/Users/rana7082/Dropbox/global-env-change/")
+#getwd()
+file_list <- list.files("data/")  
+
+
+numfiles <- length(file_list)
+
+
+library(plyr)
+dataset <- ldply(file_list, read.csv, header=TRUE, sep=",")
+
+
+
+
+
 
 
 #then stack them all in large dataframe
