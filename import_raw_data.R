826aa11ff811c4af21a75f23b8d105818349c19c
nagyrc <nagyrc@gmail.com>
2018-05-11 14:59:51 -0600

    deleted OPTION 2

--- import_raw_data.R
+++ import_raw_data.R
@@ -11,7 +11,6 @@ file_list <- list.files('data',
                         pattern = '.csv',
                         full.names = TRUE )  
 
-#OPTION 1
 ###################################################
 ###test with Nate's code to add field with part of file name
 
@@ -50,78 +49,3 @@ unique(cleaned$GCC)
 sum(cleaned$GCC == "YES")
 #this no longer works
 
-###################################################
-
-
-
-
-
-######################################
-#OPTION 2
-#read in and merge all data files
-dataset <- ldply(file_list, read.csv, header = TRUE, sep = ",")
-
-ccc <- colnames(dataset)
-ccc
-
-###
-#this is a check; do not need to run every time
-#look at the set up in the first .csv file
-#Abbott<-read.csv("001AbbottLD.csv", header = TRUE, sep = ",", fill = TRUE)
-#head(Abbott)
-###
-
-
-###
-#cleaning data
-#sort by those that Caelan grouped into related to GEC or not
-summary(dataset$GC)
-#roughly ~600 yes's, but this field needs to be cleaned...many weird ones in here
-
-#489 variables looks like a lot...let's look at all field (column) names
-colnames(dataset)
-#there are many that are labeled as X.1-X.419...what are these?
-#then EA and EY at the end...these look real
-
-summary(dataset$X.1)
-
-head(dataset)
-
-#look at the end to make sure they are all legit
-tail(dataset)
-#they look fine, minus those weird X columns
-
-
-###cleaning the data
-#drop the weird X columns
-data2 <- dataset[,-grep(pattern="^X.", colnames(dataset))]
-
-colnames(data2)
-#check this against column names to make sure I didn't delete any real info.
-#GC is a column that Caelan added- his designation of related to global change or not
-
-summary(data2$GC)
-head(data2)
-
-#cleaning GC column
-str(data2$GC)
-data2$GCC <- as.character(data2$GC)
-is.character(data2$GCC)
-
-cleaned <- data2 %>%
-  mutate(GCC = ifelse(GC == 'Yes' | GC == 'Yes ' | GC == 'yes' | GC == 'yes?' | GC == 'Yes?'| GC == 'YEs', 'YES',
-                      ifelse(GC == 'old', 'OLD',
-                             ifelse(GC == ' ' | GC == '' | GC == 'maybe' | GC == 'Maybe' | GC == 'Maybe?' | GC == 't', 'MAYBE',
-                                    ifelse(GC == 'Nope' | GC == 'NOPE', 'NO', as.character(GC))))))
-
-sum(cleaned$GCC == "YES")
-#614
-sum(cleaned$GCC == "NO")
-#3545
-sum(cleaned$GCC == "MAYBE")
-#6
-sum(cleaned$GCC == "OLD")
-#9821
-
-unique(cleaned$GCC)
-#now it only has yes, no, old, and maybe
