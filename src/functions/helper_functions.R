13b9616b7c380aa997bc126da6aed850539fe1a4
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-11 12:46:54 -0600

    first commit

---
+++ src/functions/helper_functions.R
@@ -0,0 +1,7 @@
+
+clean_gc <- function(GC) {
+  ifelse(GC == 'Yes' | GC == 'Yes ' | GC == 'yes' | GC == 'yes?' | GC == 'Yes?' | GC == 'YEs', 'YES',
+         ifelse(GC == 'old', 'OLD',
+                ifelse(GC == ' ' | GC == '' | GC == 'maybe' | GC == 'Maybe' | GC == 'Maybe?' | GC == 't' | is.na(GC), 'MAYBE',
+                       ifelse(GC == 'Nope' | GC == 'NOPE', 'NO', as.character(GC)))))
+}
