1248ffe7500939528bfcdd45aa8a436d05cdf2e6
nagyrc <nagyrc@gmail.com>
2018-05-25 13:59:48 -0600

    cleanzzz

--- import_raw_data.R
+++ import_raw_data.R
@@ -15,14 +15,8 @@ file_list <- list.files('data',
 #it doesn't recognize "EA" and "EY" as columns...remove these and see if it works...these are early access data- not super important
 keep = c("GC", "PT", "AU", "BA", "BE", "GP", "AF", "BF", "CA", "TI", "SO", "SE", "BS", "LA", "DT", "CT", "CY", "CL", "SP", "HO", "DE", "ID", "AB", "C1", "RP", "EM", "RI", "OI", "FU", "FX", "CR", "NR", "TC", "Z9", "U1", "U2", "PU", "PI", "PA", "SN", "EI", "BN", "J9", "JI", "PD", "PY", "VL", "IS", "PN", "SU", "SI", "MA", "BP", "EP", "AR", "DI", "D2", "PG", "WC", "SC", "GA", "UT", "PM", "OA", "HC", "HP", "DA", "file_names")
 
-clean_gc <- function(GC) {
-  ifelse(GC == 'Yes' | GC == 'Yes ' | GC == 'yes' | GC == 'yes?' | GC == 'Yes?' | GC == 'YEs', 'YES',
-         ifelse(GC == 'old', 'OLD',
-                ifelse(GC == ' ' | GC == '' | GC == 'maybe' | GC == 'Maybe' | GC == 'Maybe?' | GC == 't' | is.na(GC), 'MAYBE',
-                       ifelse(GC == 'Nope' | GC == 'NOPE', 'NO', as.character(GC)))))
-}
 
-#applies a function to all the files in file_list, cleans the GC field, and adds a field that indicates the author name
+#applies a function to all the files in file_list and adds a field that indicates the author name
 imported_csv <- lapply(file_list,
                        FUN = function(x) {
                          
@@ -36,7 +30,15 @@ imported_csv <- lapply(file_list,
 #bind all of these together in one dataframe
 imported_csv <- do.call(rbind, imported_csv)
 
-#this uses the helper function (clean_gc) that Nate created and calls in source above
+#this is a function that will clean the GC field because there is much variation in these factors
+clean_gc <- function(GC) {
+  ifelse(GC == 'Yes' | GC == 'Yes ' | GC == 'yes' | GC == 'yes?' | GC == 'Yes?' | GC == 'YEs', 'YES',
+         ifelse(GC == 'old', 'OLD',
+                ifelse(GC == ' ' | GC == '' | GC == 'maybe' | GC == 'Maybe' | GC == 'Maybe?' | GC == 't' | is.na(GC), 'MAYBE',
+                       ifelse(GC == 'Nope' | GC == 'NOPE', 'NO', as.character(GC)))))
+}
+
+#this uses the function above to clean the GC field and creates a new column with character values for the GC field
 cleaned <- imported_csv %>%
   mutate(GCC = clean_gc(as.character(GC)))
 
